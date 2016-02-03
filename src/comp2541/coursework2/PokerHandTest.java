4bb302628322aa2a601b1b8f59d25d27182b63d1
thomas <thomas@MyDestopPC>
2016-02-03 17:36:42 +0000

    PokerHandtest included outlined tests

---
+++ src/comp2541/coursework2/PokerHandTest.java
@@ -0,0 +1,31 @@
+package comp2541.coursework2;
+
+import java.util.List;
+
+public class PokerHandTest {
+
+private List<Card> testHand;
+
+	@Test
+	public void sizeTest()
+	{
+		//check that only 5 cards can be held in a hand 
+		// at any time
+	}
+	
+	@Test
+	public void toStringTest()
+	{
+		//check the to string method works
+		
+	}
+	
+	@Test
+	public void flushTest()
+	{
+		//check the isFkush method works
+	
+	}
+	
+	
+}
