5a2de24a3dd904eeab664651730d5f8a1faac95f
thomas <thomas@MyDestopPC>
2016-02-03 18:08:56 +0000

    size test implimented with error

--- src/comp2541/coursework2/PokerHandTest.java
+++ src/comp2541/coursework2/PokerHandTest.java
@@ -1,16 +1,46 @@
 package comp2541.coursework2;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class PokerHandTest {
 
 private List<Card> testHand;
 
-	@Test
+private Card aceClubs;
+private Card twoClubs;
+private Card twoDiamonds;
+private Card jackHearts;
+private Card kingSpades;
+private Card tenHearts;
+
+@Before public void setUp()
+{
+	testHand = new ArrayList<Card>();
+  aceClubs = new Card("AC");
+  twoClubs = new Card("2C");
+  twoDiamonds = new Card("2D");
+  jackHearts = new Card("JH");
+  kingSpades = new Card("KS");
+  tenHearts = new Card("TH");
+}
+
+	@Test(expected=IllegalArgumentException.class)
 	public void sizeTest()
 	{
 		//check that only 5 cards can be held in a hand 
 		// at any time
+		testHand.add(aceClubs);
+		testHand.add(twoClubs);
+		testHand.add(twoDiamonds);
+		testHand.add(jackHearts);
+		testHand.add(tenHearts);
+		testHand.add(kingSpades);
+		
+		
 	}
 	
 	@Test
