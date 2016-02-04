9765fc32f7393bb12879fbd8676754eda0030d5e
Thomas Crowley <sc14talc@leeds.ac.uk>
2016-02-04 11:20:13 +0000

    is flush test implimented

--- src/comp2541/coursework2/PokerHandTest.java
+++ src/comp2541/coursework2/PokerHandTest.java
@@ -18,6 +18,9 @@ private Card twoDiamonds;
 private Card jackHearts;
 private Card kingSpades;
 private Card tenHearts;
+private Card threeClubs;
+private Card sevenClubs;
+private Card nineClubs;
 
 @Before public void setUp()
 {
@@ -28,6 +31,10 @@ private Card tenHearts;
   jackHearts = new Card("JH");
   kingSpades = new Card("KS");
   tenHearts = new Card("TH");
+  threeClubs = new Card("3C");
+  sevenClubs= new Card("7C");
+  nineClubs = new Card("9C");
+  
 }
 
 	@Test(expected=IllegalArgumentException.class)
@@ -62,12 +69,36 @@ private Card tenHearts;
 		assertEquals("three cards:", "AC, 2C, 2D", testHand.toString());
 	}
 	@Test
-	public void flushTest()
+	public void isflushTest()
 	{
 		//check the isFlush method works
+		testHand.pokerAdd(aceClubs);
+		testHand.pokerAdd(twoClubs);
+		testHand.pokerAdd(sevenClubs);
+		testHand.pokerAdd(nineClubs);
+		testHand.pokerAdd(threeClubs);
+		
+		assertTrue(testHand.isFlush());
 	
 	}
 	
+	@Test
+	public void notFlushTest()
+	{
+		// check not flush works
+		testHand.pokerAdd(kingSpades);
+		testHand.pokerAdd(jackHearts);
+		testHand.pokerAdd(sevenClubs);
+		testHand.pokerAdd(nineClubs);
+		testHand.pokerAdd(twoDiamonds);
+		
+		assertFalse(testHand.isFlush());
+	}
+	
+
+	
+	
+
 	@Test
 	public void threeOfKindTest()
 	{
